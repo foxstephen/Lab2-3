5cda452a9b6deda7b9c58285bd004458dc1247e8
stephenfox1995 <stephenfox995@hotmail.com>
2015-09-30 14:54:41 +0000

    Check if the word is a palindrome.

--- LabThree/palindrome.py
+++ LabThree/palindrome.py
@@ -3,6 +3,7 @@ inputString = input("Enter a word:")
 def reversedString(someString):
     return someString[::-1]
 
-
-print(reversedString(inputString))
-
+if inputString == reversedString(inputString):
+  print("This word is a palidrome")
+else:
+  print("This word is not a palindrome")
